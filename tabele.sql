3b66804676c13f8174a2976aaf45b6d1b312c857
ErykPalasz <eryk.palasz@gmail.com>
2019-01-19 22:50:42 +0100

    lepsze drop table i poprawione pozostałe bugi

--- tabele.sql
+++ tabele.sql
@@ -1,13 +1,13 @@
-DROP TABLE dane_osobowe;
-DROP TABLE rodzaj_biletu;
-DROP TABLE seanse;
-DROP TABLE filmy;
-DROP TABLE gatunki;
-drop table klienci;
-drop table sprzedawcy;
-drop table bilety;
-drop table miejsca;
-drop table rezyserzy;
+DROP TABLE dane_osobowe CASCADE CONSTRAINTS PURGE;
+DROP TABLE rodzaj_biletu CASCADE CONSTRAINTS PURGE;
+DROP TABLE seanse CASCADE CONSTRAINTS PURGE;
+DROP TABLE filmy CASCADE CONSTRAINTS PURGE;
+DROP TABLE gatunki CASCADE CONSTRAINTS PURGE;
+drop table klienci CASCADE CONSTRAINTS PURGE;
+drop table sprzedawcy CASCADE CONSTRAINTS PURGE;
+drop table bilety CASCADE CONSTRAINTS PURGE;
+drop table miejsca CASCADE CONSTRAINTS PURGE;
+drop table rezyserzy CASCADE CONSTRAINTS PURGE;
 
 CREATE TABLE dane_osobowe(
    id_dane_osobowe VARCHAR(10) NOT NULL, 
@@ -19,11 +19,11 @@ CREATE TABLE dane_osobowe(
 ALTER TABLE dane_osobowe ADD PRIMARY KEY(id_dane_osobowe);
 
 CREATE TABLE rodzaj_biletu(
-   id_biletu VARCHAR(10) NOT NULL,
+   id_rodzaj_biletu VARCHAR(10) NOT NULL,
    rodzaj VARCHAR(10) NOT NULL,
    cena NUMERIC(4) NOT NULL
 );
-ALTER TABLE rodzaj_biletu ADD PRIMARY KEY(id_biletu);
+ALTER TABLE rodzaj_biletu ADD PRIMARY KEY(id_rodzaj_biletu);
 
 CREATE TABLE seanse(
    id_seansu VARCHAR(10) NOT NULL,
@@ -54,55 +54,53 @@ create table klienci(
 );
 alter table klienci add primary key(id_klient);
 
-create table sprzedawcy
-(
+create table sprzedawcy(
    id_sprzedawca VARCHAR2(10) NOT NULL, 
-   id_dane_osobowe VARCHAR2(10) NOT NULL, 
+   id_dane_osobowe VARCHAR2(10) NOT NULL,
+   pesel VARCHAR(11) NOT NULL,
    data_zatrudnienia DATE, 
    data_zwolnienia DATE
 );
 alter table sprzedawcy add primary key(id_sprzedawca);
 
-create table miejsca
-(
-id_miejsca VARCHAR(10) NOT NULL,
-rzad_litera CHAR(2) NOT NULL, 
-fotel_cyfra INTEGER(2) NOT NULL
+create table miejsca(
+   id_miejsca VARCHAR(10) NOT NULL,
+   rzad_litera CHAR(2) NOT NULL, 
+   fotel_cyfra NUMERIC(2) NOT NULL
 );
 alter table miejsca add primary key(id_miejsca);
 
-create table bilety
-(
-id_biletu VARCHAR(10) NOT NULL,
-id_miejsca VARCHAR(10) NOT NULL,
-id_klient VARCHAR(10) NOT NULL,
-id_sprzedawca VARCHAR(10) NOT NULL,
-id_seans VARCHAR(10) NOT NULL,
-id_rodzaj_biletu VARCHAR(10) NOT NULL,
-data_kupna DATETIME(16),
-termin_waznosci DATETIME(16)
+create table bilety  (
+   id_biletu VARCHAR(10) NOT NULL,
+   id_miejsca VARCHAR(10) NOT NULL,
+   id_klient VARCHAR(10) NOT NULL,
+   id_sprzedawca VARCHAR(10) NOT NULL,
+   id_seansu VARCHAR(10) NOT NULL,
+   id_rodzaj_biletu VARCHAR(10) NOT NULL,
+   data_kupna DATE,
+   termin_waznosci DATE
 );
 alter table bilety add primary key(id_biletu);
 
-create table rezyserzy
-(
-id_rezyser VARCHAR2(10) NOT NULL,
-imie VARCHAR2(20) NOT NULL,
-nazwisko VARCHAR2(20) NOT NULL
+create table rezyserzy(
+   id_rezyser VARCHAR2(10) NOT NULL,
+   imie VARCHAR2(20) NOT NULL,
+   nazwisko VARCHAR2(20) NOT NULL
 );
-alter table bilety add primary key(id_rezyser);
+alter table rezyserzy add primary key(id_rezyser);
 
 alter table klienci add foreign key(id_dane_osobowe) REFERENCES dane_osobowe(id_dane_osobowe);
 alter table sprzedawcy add foreign key(id_dane_osobowe) REFERENCES dane_osobowe(id_dane_osobowe);
 alter table bilety add foreign key(id_miejsca) REFERENCES miejsca(id_miejsca);
 alter table bilety add foreign key(id_klient) REFERENCES klienci(id_klient);
 alter table bilety add foreign key(id_sprzedawca) REFERENCES sprzedawcy(id_sprzedawca);
-alter table bilety add foreign key(id_seans) REFERENCES seanse(id_seans);
+alter table bilety add foreign key(id_seansu) REFERENCES seanse(id_seansu);
 alter table bilety add foreign key(id_rodzaj_biletu) REFERENCES rodzaj_biletu(id_rodzaj_biletu);
 
-alter table seanse add foreign key(id_film) REFERENCES filmy(id_film);
+alter table seanse add foreign key(id_filmu) REFERENCES filmy(id_filmu);
 alter table filmy add foreign key(id_rezyser) REFERENCES rezyserzy(id_rezyser);
-alter table filmy add foreign key(id_gatunek) REFERENCES gatunki(id_gatunek);
+alter table filmy add foreign key(id_gatunku) REFERENCES gatunki(id_gatunku);
+
 
 
 
