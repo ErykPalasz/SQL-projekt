250f11419a51ad18d587556545ea4f64b4763c0e
blanko124 <wojtek.rybacki2@onet.pl>
2019-01-20 15:26:10 +0100

    inserty dane osobowe

--- inserty.sql
+++ inserty.sql
@@ -6,6 +6,42 @@ insert into rezyserzy (imie, nazwisko) values ('Rivalee', 'Prier');
 insert into rezyserzy (imie, nazwisko) values ('Errol', 'Coyte');
 
 -- gatunki
+insert into gatunki (nazwa) values ('Drama');
+insert into gatunki (nazwa) values ('Crime');
+insert into gatunki (nazwa) values ('Comedy');
+insert into gatunki (nazwa) values ('Romance');
+insert into gatunki (nazwa) values ('Horror');
 
+-- dane osobowe
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Rolf', 'Trudgeon', 'rtrudgeon0@reference.com', '865 860 152');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Bren', 'Quarrie', 'bquarrie1@cdbaby.com', '998 687 451');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Malory', 'McNeil', 'mmcneil2@cam.ac.uk', '654 217 272');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Henriette', 'Eves', 'heves3@dell.com', '679 164 189');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Nissie', 'Averay', 'naveray4@godaddy.com', '561 876 684');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Melosa', 'Druery', 'mdruery5@microsoft.com', '331 597 920');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Edd', 'MacMeanma', 'emacmeanma6@mlb.com', '451 116 253');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Susanne', 'Levine', 'slevine7@rakuten.co.jp', '118 320 517');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Ernst', 'Celez', 'ecelez8@flickr.com', '763 562 141');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Scotti', 'Baile', 'sbaile9@marketwatch.com', '838 754 223');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Regen', 'Cotesford', 'rcotesforda@mayoclinic.com', '742 122 155');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Arnold', 'Laible', 'alaibleb@storify.com', '762 247 243');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Geno', 'Bushell', 'gbushellc@github.com', '701 219 828');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Petr', 'Russan', 'prussand@meetup.com', '194 287 947');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Clayborne', 'Chainey', 'cchaineye@ed.gov', '306 591 199');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Mickie', 'Wyrall', 'mwyrallf@printfriendly.com', '713 277 470');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Faina', 'Asch', 'faschg@stumbleupon.com', '569 525 555');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Dilan', 'Bevis', 'dbevish@surveymonkey.com', '541 747 471');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Yasmeen', 'Stansfield', 'ystansfieldi@ning.com', '549 473 105');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Addi', 'McAlinion', 'amcalinionj@163.com', '337 803 649');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Suzanne', 'Skewis', 'sskewisk@alibaba.com', '676 417 691');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Dillon', 'Peinke', 'dpeinkel@lycos.com', '372 912 732');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Khalil', 'Grindall', 'kgrindallm@buzzfeed.com', '836 807 132');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Jenifer', 'Gallichan', 'jgallichann@goodreads.com', '194 458 632');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Hephzibah', 'McCart', 'hmccarto@bandcamp.com', '861 164 900');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Roselia', 'Maestro', 'rmaestrop@nps.gov', '360 341 192');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Brigitte', 'McCauley', 'bmccauleyq@usatoday.com', '360 649 334');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Fredi', 'Graundisson', 'fgraundissonr@arizona.edu', '684 257 575');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Tonnie', 'Sapp', 'tsapps@comsenz.com', '285 265 123');
+insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Arabele', 'Crowth', 'acrowtht@dropbox.com', '500 948 072');
 
-insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Marek', 'Stypa', 'marek.stypa@mail.pl', '111111111');
+--
