90993004c30093597ecc7381981ce9421811fba7
blanko124 <wojtek.rybacki2@onet.pl>
2019-01-20 20:12:55 +0100

    sztuczna data do seansow

---
+++ inserty/v seanse.sql
@@ -0,0 +1,7 @@
+insert into seanse (id_filmu, data_godzina) values ('ThC72EC74REZ72', '1900/1/1 09:00:00');
+insert into seanse (id_filmu, data_godzina) values ('22H74TA72REZ73', '1900/1/1 11:00:00');
+insert into seanse (id_filmu, data_godzina) values ('TeC72TA72REZ74', '1900/1/1 13:00:00');
+insert into seanse (id_filmu, data_godzina) values ('ThM76BA70REZ75', '1900/1/1 15:00:00');
+insert into seanse (id_filmu, data_godzina) values ('BoD70BA70REZ76', '1900/1/1 17:00:00');
+insert into seanse (id_filmu, data_godzina) values ('BeH74RP73REZ77', '1900/1/1 19:00:00');
+insert into seanse (id_filmu, data_godzina) values ('StC71LM71REZ78', '1900/1/1 21:00:00');
