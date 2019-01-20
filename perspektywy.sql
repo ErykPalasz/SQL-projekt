400fd6acae0c9dbd55d1560fc9313e000a9adb8d
ErykPalasz <eryk.palasz@gmail.com>
2019-01-20 23:53:01 +0100

    bilet

--- perspektywy.sql
+++ perspektywy.sql
@@ -1,1 +1,5 @@
 -- minimum 6
+
+create view bilet_druk as (select bilet.id_biletu, seans.id_filmu, miejsca.id_miejsca, bilet.data_kupna, bilet.rodzaj_biletu
+from bilet, filmy, miejsca, seanse
+where bilet.id_seansu = seans.id_filmu and bilet.id_miejsca = miejsce.id_miejsca and data_kupna = data_kupna and rodzaj_biletu = rodzaj_biletu) 
