b8001d022ecba819bfd7de38608a1231ff542ed8
ErykPalasz <eryk.palasz@gmail.com>
2019-01-20 11:07:24 +0100

    usuniecie tablicy 'rodzaj_biletu'

--- tabele.sql
+++ tabele.sql
@@ -105,10 +105,6 @@ CREATE sequence id_dane_osobowe_seq
 minvalue 1 maxvalue 999999999999999999999999999
 increment by 1 start with 70 cache 20 noorder nocycle;
 
-CREATE sequence id_rodzaj_biletu_seq
-minvalue 1 maxvalue 999999999999999999999999999
-increment by 1 start with 70 cache 20 noorder nocycle;
-
 CREATE sequence id_seansu_seq
 minvalue 1 maxvalue 999999999999999999999999999
 increment by 1 start with 70 cache 20 noorder nocycle;
