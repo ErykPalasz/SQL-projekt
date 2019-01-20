5831c207440153e6680ceb0e0237e1c69b96535e
ErykPalasz <eryk.palasz@gmail.com>
2019-01-20 10:33:35 +0100

    sekwencje

--- tabele.sql
+++ tabele.sql
@@ -108,7 +108,45 @@ CREATE index indeks_bilet_miejsce_seans_wazny on bilety(id_miejsca, id_seansu, t
 CREATE index indeks_miejsce_rzad_fotel on miejsca(rzad_litera, fotel_cyfra);
 
 -- sekwencje
-CREATE sequence id
+CREATE sequence id_dane_osobowe_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_rodzaj_biletu_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_seansu_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_filmu_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_gatunku_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_klient_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_sprzedawca_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_miejsca_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_biletu_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_rezyser_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
 
 -- trigery
 
