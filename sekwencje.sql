9f4c234d863c7042a04d4e0e72396c8fc9917576
ErykPalasz <eryk.palasz@gmail.com>
2019-01-20 10:31:15 +0100

    nowe sekwencje

---
+++ sekwencje.sql
@@ -0,0 +1,39 @@
+CREATE sequence id_dane_osobowe_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_rodzaj_biletu_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_seansu_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_filmu_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_gatunku_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_klient_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_sprzedawca_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_miejsca_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_biletu_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
+
+CREATE sequence id_rezyser_seq
+minvalue 1 maxvalue 999999999999999999999999999
+increment by 1 start with 70 cache 20 noorder nocycle;
