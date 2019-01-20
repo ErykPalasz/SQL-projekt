7118db8ba6c6e4f2dbb1cb90bb6de409f53b1665
blanko124 <wojtek.rybacki2@onet.pl>
2019-01-20 19:50:17 +0100

    inserty seanse

---
+++ inserty/seanse.sql
@@ -0,0 +1,8 @@
+insert into seanse (id_filmu, data_godzina) values ('ThC72EC74REZ72', '09:00:00');
+insert into seanse (id_filmu, data_godzina) values ('22H74TA72REZ73', '11:00:00');
+insert into seanse (id_filmu, data_godzina) values ('TeC72TA72REZ74', '13:00:00');
+insert into seanse (id_filmu, data_godzina) values ('ThM76BA70REZ75', '15:00:00');
+insert into seanse (id_filmu, data_godzina) values ('BoD70BA70REZ76', '17:00:00');
+insert into seanse (id_filmu, data_godzina) values ('BeH74RP73REZ77', '19:00:00');
+insert into seanse (id_filmu, data_godzina) values ('StC71LM71REZ78', '21:00:00');
+insert into seanse (id_filmu, data_godzina) values ('HaH74LM71REZ79', '22:00:00');
