e7d31203a40615e84ceb0f596e6cc20046b1ee64
blanko124 <wojtek.rybacki2@onet.pl>
2019-01-20 15:11:44 +0100

    nowe inserty

--- inserty.sql
+++ inserty.sql
@@ -1,1 +1,11 @@
+-- rezyserzy 
+insert into rezyserzy (imie, nazwisko) values ('Brit', 'Alvin');
+insert into rezyserzy (imie, nazwisko) values ('Langsdon', 'MacKim');
+insert into rezyserzy (imie, nazwisko) values ('Tedi', 'Aris');
+insert into rezyserzy (imie, nazwisko) values ('Rivalee', 'Prier');
+insert into rezyserzy (imie, nazwisko) values ('Errol', 'Coyte');
+
+-- gatunki
+
+
 insert into dane_osobowe (imie, nazwisko, email, telefon) values ('Marek', 'Stypa', 'marek.stypa@mail.pl', '111111111');
