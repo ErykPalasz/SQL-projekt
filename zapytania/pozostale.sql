07b110ec57bd9d8b4927137abc47af4ebc10a901
ErykPalasz <eryk.palasz@gmail.com>
2019-01-28 13:47:23 +0100

    Revert "."
    This reverts commit 8f01d1c5d7748a365ec2fc72d971495457a77ec0.

---
+++ zapytania/pozostale.sql
@@ -0,0 +1,7 @@
+-- iv. elementy interakcyjne w poleceniach SQL – parametryzacja poleceń SQL
+-- v. agregowania danych (użycie frazy group by) – na podstawie jednej lub wielu tabel
+-- vi. agregowania danych (użycie frazy having) – na podstawie jednej lub wielu tabel
+-- vii. wykorzystujące funkcje związane z obsługą daty i czasu, arytmetyczne, znakowe
+-- viii. zapytania zagnieżdżone
+-- ix. zapytania zagnieżdżone z agregowaniem danych (rollup, cube)
+-- x. utworzenie tabeli tymczasowej na podstawie jednej lub wielu tabel
